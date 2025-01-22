64dea772ab4ae6b4cdf2c72ad56f14bf475b60a8
Nick <susdev999@gmail.com>
2025-01-22 15:11:42 +0900

    typo fixes

--- database/migrations/2025_01_19_111218_create_categories_table.php
+++ database/migrations/2025_01_19_111218_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug')->uniqid;
+            $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->boolean('status')->default(1);
             $table->timestamps();
